83d0b369ea4738553b48af9a85963c257aee9899
MurisonWork <Alexander.Murison@icr.ac.uk>
2014-01-02 12:25:30 +0000

    Update new_phylo.R
    added error check
    correctly can specify number of root nodes

--- R/new_phylo.R
+++ R/new_phylo.R
@@ -1,5 +1,6 @@
 ## Generates phylogenies with one or more root nodes
 new_phylo<-function(nroot,number_of_clones) {
+  if (nroot > number_of_clones) {stop("Terminating Script: Number of roots cannot be greater than number of clones")}
   inp<-vector(length=number_of_clones)
   
   ## Maximum number of roots is the number of clones
@@ -8,14 +9,17 @@ new_phylo<-function(nroot,number_of_clones) {
   ## When nroot is 0, we randomise the number of roots 
   if (nroot==0) { 
     i<-round(runif(1,0.5, number_of_clones+0.49)) 
+    inp[1:i]<-0
+    last_gen_end<-i
+    spaces_left<-number_of_clones-i
+  } else { 
+    inp[1:nroot]<-0
+    last_gen_end<-nroot
+    spaces_left<-number_of_clones-nroot
   }
-    
-  inp[1:i]<-0
   
   last_gen_start<-1
-  last_gen_end<-i
-  spaces_left<-number_of_clones-i
-  
+   
   while (spaces_left > 0) {
     length_next_gen<-round(runif(1,0.5, spaces_left+0.49))
     current_sample<-sample(rep(last_gen_start:last_gen_end, number_of_clones))
