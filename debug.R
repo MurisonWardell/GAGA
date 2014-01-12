a4f5c370ca74918a3668f46371a3a7d44e4b2c4d
WardellHome <christopher.wardell@icr.ac.uk>
2014-01-12 18:00:22 +0000

    Started writing Vignette.  Not done by a loooong way.

--- debug.R
+++ debug.R
@@ -7,6 +7,8 @@ library(GAGA)
 
 ## Load synthetic data set
 #data("gaga_synthetic_data","gaga_synthetic_data_annotation")
+data("hidden_gaga")
+data("gaga_simple_data")
 data("gaga_synthetic_data")
 data("gaga_synthetic_data_jittered")
 data("BYB1_G07_pruned")
@@ -18,6 +20,11 @@ gdata=gaga(gaga_synthetic_data, number_of_clones=6, iterations=3000)
 gdataj=gaga(gaga_synthetic_data_jittered, number_of_clones=6, iterations=3000)
 gagaYeast=gaga(BYB1_G07_pruned, number_of_clones=6, iterations=1000)
 gagaYeastC=gaga(BYB1_G07_pruned, number_of_clones=6, iterations=1000,contamination=1)
+
+## Correct solution for 
+simpleDataSolution=gaga(gaga_simple_data, number_of_clones=4, nroot=0,iterations=3000)
+simpleDataSolution=gaga(gaga_simple_data, number_of_clones=4, nroot=1,iterations=500) # will hit correct solution faster
+
 
 gagaReport(gaga_synthetic_data,gdata,output_file_prefix="gaga_synthetic_data")
 gagaReport(gaga_synthetic_data_jittered,gdataj,output_file_prefix="gaga_synthetic_data_jittered")
@@ -41,3 +48,10 @@ gagaReport(gaga_synthetic_data,z,outType="proportion")
 gagaReport(gaga_synthetic_data,z,outType="phylogeny")
 
 setwd("N:/MORGAN/ChrisW/documents/projects/131213_gaga/GAGA")
+
+
+## The horrifying building vignettes section:
+library(devtools)
+build_vignettes()
+
+
